c4bd20f87971333ab0851bfcacdfddb1aee523de
Glitch (hello-webpage) <none>
2020-03-23 22:00:13 +0000

    🌛🐃 Checkpoint ./index.html:13243716/2359 ./script.js:13243716/3135

--- script.js
+++ script.js
@@ -6,46 +6,45 @@ console.log("hi");
 
 const $ = document.querySelector.bind(document);
 
-const predictionEl = $('#prediction');
+const predictionEl = $("#prediction");
 
 const wizard = new tm.Wizard({
-    classes: [
-        {
-            name: 'Yes',
-            title: '“Yes” examples.',
-            description: 'Add examples of what you want to trigger your “yes” state. This can be anything you want, like holding up your hand or an object.'
-        },
-        {
-            name: 'No',
-            title: '“No” examples.',
-            description: 'Add examples of what you want to trigger your “no” state. For example, without your hand or object.'
-        }
-    ],
-    onLoad: () => {
-        console.log('model has loaded');
+  classes: [
+    {
+      name: "Yes",
+      title: "“Yes” examples.",
+      description:
+        "Add examples of what you want to trigger your “yes” state. This can be anything you want, like holding up your hand or an object."
     },
-    onPrediction: (predictions) => {
-        predictions.sort((a, b) => a.probability > b.probability ? -1 : 1);
-        predictionEl.innerHTML = predictions[0].className;
-    },
-    onSampleAdded: (added) => {
-        console.log(added);
-    },
-    onTrain: () => console.log('train begins'),
-    onTrainComplete: () => {
-        console.log('train complete');
-        $('#intro-copy').classList.add('hidden');
-        $('#confirmation-copy').classList.remove('hidden');
-        const inferenceCamera = wizard.createInferenceCamera();
-        document.body.appendChild(inferenceCamera);
+    {
+      name: "No",
+      title: "“No” examples.",
+      description:
+        "Add examples of what you want to trigger your “no” state. For example, without your hand or object."
     }
+  ],
+  onLoad: () => {
+    console.log("model has loaded");
+  },
+  onPrediction: predictions => {
+    predictions.sort((a, b) => (a.probability > b.probability ? -1 : 1));
+    predictionEl.innerHTML = predictions[0].className;
+  },
+  onSampleAdded: added => {
+    console.log(added);
+  },
+  onTrain: () => console.log("train begins"),
+  onTrainComplete: () => {
+    console.log("train complete");
+    $("#intro-copy").classList.add("hidden");
+    $("#confirmation-copy").classList.remove("hidden");
+    const inferenceCamera = wizard.createInferenceCamera();
+    document.body.appendChild(inferenceCamera);
+  }
 });
-
 
 // document.body.appendChild(wizard.domElement);
 document.body.appendChild(wizard.buttonElement);
 
 // console.log('@teachablemachine/api wizard: ' ,new tm.Wizard());
 // console.log('yeae boi');
-
-
